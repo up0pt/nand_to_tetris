20924feac0471354799c04c3cfba5e813459de9e
up0pt <giftmath01au@gmail.com>
2026-02-13 21:44:10 +0900

    add: program_flow [not tested yet]

--- vm_translator/src/commands/program_flow.py
+++ vm_translator/src/commands/program_flow.py
@@ -1,19 +1,43 @@
 from dataclasses import dataclass
+import re
+from typing import ClassVar
+
+from command_kind import VmCmd
+
+@dataclass(frozen=True)
+class Label(VmCmd):
+    vm_op: ClassVar[str] = "label"
+    label_name: str
+
+    def asm_lines(self, label_id: str, file_name: str):
+        if re.fullmatch(r"^(?!\d)[a-zA-Z0-9_.:]+", self.label_name):
+            return f"""
+({file_name}.{self.label_name})
+            """
+        else:
+            raise ValueError("Label name should not start with digit and as a whole, it should use only digit, alphabet (Capitalized or lower), or symbols (_.:)")
 
 
 @dataclass(frozen=True)
-class Label:
-    symbol: str
-    op_str: str = "label"
+class Goto(VmCmd):
+    vm_op: ClassVar[str] = "goto"
+    distination: str
+    def asm_lines(self, label_id: str, file_name: str) -> str:
+        return f"""
+@{file_name}.{self.distination}
+0;JMP
+        """
 
 
 @dataclass(frozen=True)
-class Goto:
-    symbol: str
-    op_str: str = "goto"
-
-
-@dataclass(frozen=True)
-class If:
-    symbol: str
-    op_str: str = "if-goto"
+class If(VmCmd):
+    vm_op: ClassVar[str] = "if-goto"
+    distination: str
+    def asm_lines(self, label_id: str, file_name: str) -> str:
+        return f"""
+@SP
+AM=M-1
+D=M
+@{file_name}.{self.distination}
+D;JMP
+"""
