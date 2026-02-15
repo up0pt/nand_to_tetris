e0b6f7a6bff1800bd7faa98d48743a386e23f4d4
up0pt <giftmath01au@gmail.com>
2026-02-15 19:57:00 +0900

    minor-fix: remove comment

--- vm_translator/src/commands/__init__.py
+++ vm_translator/src/commands/__init__.py
@@ -1,5 +1,4 @@
 def register_all_VmCmd() -> None:
-    # ここで import する（関数内importはこの用途で普通にアリ）
     import commands.arithmetic # pyright: ignore[reportUnusedImport] # noqa: F401
     import commands.function_call # pyright: ignore[reportUnusedImport] # noqa: F401
     import commands.memory_access # pyright: ignore[reportUnusedImport] # noqa: F401
